8b07c077de6f61922dc7c74bfaaeb9a87b1d22a4
lchipl <52299956+lchipl@users.noreply.github.com>
2020-11-19 05:55:44 +0300

    'outline'

---
+++ src/components/NewsList/NewsList.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { News } from '../News';
+
+export const NewsList = () =>{
+    return(
+        <News />
+    )
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { NewsList } from '../components/NewsList/NewsList';
 
 
 export const MainPage = ()=>{
     return(
         <div>
             Главная страница
+             <NewsList />
         </div>
     )
 }
